f91bf4babb93d1c89deb00156d7f5f65b47d4623
YannickAtabong9 <151992112+YannickAtabong9@users.noreply.github.com>
2024-07-17 05:47:16 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//index.js
+
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
+
+hamburger.addEventListener('click', function () { 
+    const hamIcon = this.querySelector('.hamburger-icon'); 
+    const crossIcon = this.querySelector('.cross-icon'); 
+    if (hamIcon.style.display === "none") { 
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
+    } 
+    else { 
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
+    } 
+});
